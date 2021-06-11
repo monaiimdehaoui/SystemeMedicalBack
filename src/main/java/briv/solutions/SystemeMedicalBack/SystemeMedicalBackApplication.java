ad5de00f3507f3bbf87bc783c690a9fe23f08241
Monaiim <m.o.dehaoui@gmail.com>
2021-06-11 19:31:46 +0100

    init

---
+++ src/main/java/briv/solutions/SystemeMedicalBack/SystemeMedicalBackApplication.java
@@ -0,0 +1,13 @@
+package briv.solutions.SystemeMedicalBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeMedicalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeMedicalBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/briv/solutions/SystemeMedicalBack/SystemeMedicalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package briv.solutions.SystemeMedicalBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeMedicalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
